cd7710740ababdde41798e84b13d882cf02d314b
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-11 09:38:06 -0500

    cupcake object

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 
+console.log("good morning, objects");
+
+const cupcake = {
+  color: ["pink", "white", "brown"],
+  flavor: "chocolate",
+  size: "mini",
+  calories: 0,
+  icing: true
+};
+
+console.log("cupcake", cupcake);
