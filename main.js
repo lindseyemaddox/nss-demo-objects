adf24bc971fc077d725f97a22ab0d0afddbcee10
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-11 09:58:46 -0500

    for loop and forEach and a template string

--- main.js
+++ main.js
@@ -6,7 +6,20 @@ const cupcake = {
   flavor: "chocolate",
   size: "mini",
   calories: 0,
-  icing: true
+  icing: true,
+  eatCupcake: () => {
+    console.log("yum");
+  }
 };
 
 console.log("cupcake", cupcake);
+console.log("gimme da colors", cupcake.color);
+
+for (let i = 0; i < cupcake.color.length; i++) {
+  console.log("cupcake color", cupcake.color[i]);
+}
+
+cupcake.color.forEach(color => {
+  console.log(`this whole thing is a template string ${color}`);
+})
+
