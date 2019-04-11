1b60e6055b473ea72d6f264859489bf8fc3a8eeb
Lindsey Maddox <lindseyemaddox@gmail.com>
2019-04-11 12:13:03 -0500

    dot and square notation lightning exercises

--- main.js
+++ main.js
@@ -73,3 +73,30 @@ const familyMembers = [
 ]
 
 console.log(familyMembers);
+
+
+
+// LIGHTNING EXERCISES
+
+const empireStateBuilding = {
+  stories: 103,
+  height: 1453,
+  address: "350 Fifth Avenue, Manhattan, New York 10118",
+  squareFeet: 2768591,
+  constructionDate: 1931,
+  cost: 40948900,
+  owner: "Empire State Realty Trust",
+  eastWestLength: 424,
+  northSouthLength: 187,
+  architect: "Shreve, Lamb & Harmon"
+}
+
+// 1: Use dot notation to output all of the dimensions of the Empire State Building to the console.
+
+console.log(empireStateBuilding.height);
+
+// 2: Use square bracket notation to output the remaining 5 properties to the console. 
+// Create 5 variables first with the keys as their values. Use those variables to look up the values.
+
+const stories = empireStateBuilding["stories"];
+console.log(stories);
